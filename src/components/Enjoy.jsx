b4481fb7c17cb4f2d846dbd699842c470d044367
Lamia Fatiha <f6lamia@gmail.com>
2024-06-28 22:34:21 +0600

    positioning

--- src/components/Enjoy.jsx
+++ src/components/Enjoy.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import tv from "../assets/image/tv.png"
+import gif from "../assets"
 
 const Enjoy = () => {
   return (
-    <div className='border-b-8 border-gray-700'>
-      <div >
-          <h1>Enjoy on your TV</h1>
-          <p>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</p>
+    <div className='border-b-8 border-gray-700 grid grid-cols-2 gap-20 bg-black py-16'>
+      <div className='mt-32 ml-32'>
+          <h1 className='text-5xl font-extrabold mb-8'>Enjoy on your TV</h1>
+          <p className='text-2xl mb-8'>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</p>
       </div>
-      <div>
-        <img src={tv} alt="tv" />
+      <div className='mt-20'>
+        <img src={tv} alt="tv" className='w-96'/>
       </div>
     </div>
   )
