a0edd183901faf7974a6ff1859bb1494be647fc8
Lamia Fatiha <f6lamia@gmail.com>
2024-06-28 22:24:46 +0600

    started 2nd section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Enjoy from './components/Enjoy'
 
 const App = () => {
   return (
     <>
       <Hero />
+      <Enjoy />
     </>
   )
 }
---
+++ src/components/Enjoy.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import tv from "../assets/image/tv.png"
+
+const Enjoy = () => {
+  return (
+    <div className='border-b-8 border-gray-700'>
+      <div >
+          <h1></h1>
+          <p></p>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Enjoy
