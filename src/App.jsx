9feeee19312b86dc71abd5c53519954a568d6a6f
Lamia Fatiha <f6lamia@gmail.com>
2024-06-29 11:38:44 +0600

    Download section startted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Enjoy from './components/Enjoy'
+import Download from './components/Download'
 
 const App = () => {
   return (
     <>
       <Hero />
       <Enjoy />
+      <Download />
     </>
   )
 }
