de462161aac485cee9945c73575c81bb656c5fee
Lamia Fatiha <f6lamia@gmail.com>
2024-06-29 14:42:41 +0600

    done

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -4,13 +4,14 @@ import mobile from "../assets/image/mobile.jpg"
 const Download = () => {
   return (
     <>
-    <div className='border-b-8 border-gray-700 grid grid-cols-2 gap-10 bg-black py-32'>
+    <div className='border-b-8 border-gray-700 bg-black py-32
+      grid grid-cols-1 lg:grid-cols-2 md:grid-cols-1 gap-1 lg:gap-10 md:gap-1'>
       <div className='-mt-5 mx-auto'>
         <img src={mobile} alt="tv" className='w-96'/>
       </div>
-      <div className='mr-28'>
-          <h1 className='text-6xl font-extrabold mb-8'>Download your shows to watch offline</h1>
-          <p className='text-2xl mb-8'>Save your favorites easily and always have<br/>something to watch.</p>
+      <div className='mr-28 lg:mr-28'>
+          <h1 className='text-4xl lg:text-5xl md:text-4xl font-extrabold mb-8'>Download your shows to watch offline</h1>
+          <p className='text-md mb-2 lg:mb-8 md:mb-2 lg:text-2xl md:text-md'>Save your favorites easily and always have<br/>something to watch.</p>
       </div>
     </div>
     </>
