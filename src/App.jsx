3ab7ffeed6b73a30526ddae8fa412712f8cbe00f
Lamia Fatiha <f6lamia@gmail.com>
2024-06-29 13:05:27 +0600

    faq started

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Enjoy from './components/Enjoy'
 import Download from './components/Download'
 import Watch from './components/Watch'
 import Kids from './components/Kids'
+import Faq from './components/Faq'
 import Footer from './components/Footer'
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Download />
       <Watch />
       <Kids />
+      <Faq />
       <Footer />
     </>
   )
