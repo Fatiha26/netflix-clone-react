bc07a026918d011256d0a9853edb22ff16fe2216
Lamia Fatiha <f6lamia@gmail.com>
2024-06-29 13:19:13 +0600

    heading done

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Faq = () => {
   return (
-    <div>
-      
-    </div>
+    <div className='border-b-8 border-gray-700 gap-20 bg-black py-16 flex items-center flex-col'>
+       <h1 className='text-5xl font-extrabold mb-8'>Frequently Asked Questions</h1>
+
+
+  </div>
   )
 }
 
