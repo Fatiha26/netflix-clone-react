cbadd97aabca34fa6d7ac56421e02ccb7dbdee65
Lamia Fatiha <f6lamia@gmail.com>
2024-06-29 11:58:06 +0600

    Download section done

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import mobile from "../assets/image/mobile.jpg"
 
 const Download = () => {
   return (
     <>
-        
+    <div className='border-b-8 border-gray-700 grid grid-cols-2 gap-10 bg-black py-32'>
+      <div className='-mt-5 mx-auto'>
+        <img src={mobile} alt="tv" className='w-96'/>
+      </div>
+      <div className='mr-28'>
+          <h1 className='text-6xl font-extrabold mb-8'>Download your shows to watch offline</h1>
+          <p className='text-2xl mb-8'>Save your favorites easily and always have<br/>something to watch.</p>
+      </div>
+    </div>
     </>
   )
 }
