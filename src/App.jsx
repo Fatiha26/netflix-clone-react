6880b40ec7229cb22bbf057d856052f12c3a8611
Lamia Fatiha <f6lamia@gmail.com>
2024-06-29 12:06:08 +0600

    watch section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './components/Hero'
 import Enjoy from './components/Enjoy'
 import Download from './components/Download'
+import Watch from './components/Watch'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <Enjoy />
       <Download />
+      <Watch />
     </>
   )
 }
--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Watch = () => {
   return (
-    <div className='border-b-8 border-gray-700 grid grid-cols-2 gap-10 bg-black py-32'>
-    <div className='mr-28'>
-        <h1 className='text-6xl font-extrabold mb-8'>Download your shows to watch offline</h1>
-        <p className='text-2xl mb-8'>Save your favorites easily and always have<br/>something to watch.</p>
+    <div className='border-b-8 border-gray-700 grid grid-cols-2 gap-10 bg-black py-24'>
+    <div className='ml-32'>
+        <h1 className='text-6xl font-extrabold mb-8'>Watch everywhere</h1>
+        <p className='text-2xl mb-8'>Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV.</p>
     </div>
   </div>
   )
