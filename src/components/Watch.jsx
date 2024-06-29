4f7af0a7a2cb875253035bc43d026b60f4306dbe
Lamia Fatiha <f6lamia@gmail.com>
2024-06-29 12:01:44 +0600

    watch section content

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Watch = () => {
   return (
-  
+    <div className='border-b-8 border-gray-700 grid grid-cols-2 gap-10 bg-black py-32'>
+    <div className='mr-28'>
+        <h1 className='text-6xl font-extrabold mb-8'>Download your shows to watch offline</h1>
+        <p className='text-2xl mb-8'>Save your favorites easily and always have<br/>something to watch.</p>
+    </div>
+  </div>
   )
 }
 
